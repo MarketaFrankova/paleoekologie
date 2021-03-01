d5c07e47dd70e71628af66a8cdd8612aed1647d0
LenkaH <hubackova.lenka@gmail.com>
2021-03-01 10:14:27 +0100

    missing file

---
+++ src/content/collections.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+export const en = {
+  description:
+    "Pro vědecké účely máme k dispozici rozsáhlé sbírky pylových preparátů a semen rostlin, se kterými lze na našem pracovišti prezenčně pracovat.",
+  pollen: {
+    heading: "Referenční pylová sbírka",
+    description: (
+      <>
+        <p>
+          Sbírka pylových preparátů zahrnuje asi 3 500 druhů středoevropské
+          květeny, ale i jiných oblastí (např. Sibiř). Jedná se o acetolyzovaná
+          pylová zrna, tedy bez buněčného obsahu. Slouží jako srovnávací
+          materiál pro determinaci fosilních pylových zrn z různých typů
+          sedimentů. Využití sbírky pro určování nativních pylových zrn (s
+          buněčným obsahem, například z medu nebo z ovzduší) je možné, ale
+          omezené. Sbírka pylových preparátů je pravidelně obnovována a
+          doplňována.
+        </p>
+        <p>
+          Sbírka se nachází v Brně a v Průhonicích. Se sbírkou lze po dohodě
+          prezenčně pracovat.
+        </p>
+        <p>
+          <b>Kontakt:</b> Lydie Dudová (Brno), Helena Svitavská (Průhonice)
+        </p>
+      </>
+    ),
+  },
+  seeds: {
+    heading: "Sbírka semen a plodů",
+    description: "",
+  },
+  profile: {
+    heading: "Sbírka rašelinných profilů z České a Slovenské republiky",
+    description: "",
+  },
+};
+
+export const cz = {
+  description:
+    "Pro vědecké účely máme k dispozici rozsáhlé sbírky pylových preparátů a semen rostlin, se kterými lze na našem pracovišti prezenčně pracovat.",
+  pollen: {
+    heading: "Referenční pylová sbírka",
+    description: (
+      <>
+        <p>
+          Sbírka pylových preparátů zahrnuje asi 3 500 druhů středoevropské
+          květeny, ale i jiných oblastí (např. Sibiř). Jedná se o acetolyzovaná
+          pylová zrna, tedy bez buněčného obsahu. Slouží jako srovnávací
+          materiál pro determinaci fosilních pylových zrn z různých typů
+          sedimentů. Využití sbírky pro určování nativních pylových zrn (s
+          buněčným obsahem, například z medu nebo z ovzduší) je možné, ale
+          omezené. Sbírka pylových preparátů je pravidelně obnovována a
+          doplňována.
+        </p>
+        <p>
+          Sbírka se nachází v Brně a v Průhonicích. Se sbírkou lze po dohodě
+          prezenčně pracovat.
+        </p>
+        <p>
+          <b>Kontakt:</b> Lydie Dudová (Brno), Helena Svitavská (Průhonice)
+        </p>
+      </>
+    ),
+  },
+  seeds: {
+    heading: "Sbírka semen a plodů",
+    description: "",
+  },
+  profile: {
+    heading: "Sbírka rašelinných profilů z České a Slovenské republiky",
+    description: "",
+  },
+};
