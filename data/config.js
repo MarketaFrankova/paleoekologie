a3198bd93b4e67b0ecccb40407a6d092829d4678
LenkaH <hubackova.lenka@gmail.com>
2023-03-02 19:47:24 +0100

    config

--- data/config.js
+++ data/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  siteTitle: "Paleolab", // Site title.
-  siteUrl: "https://www.ibot.cas.cz", // Domain of your website without pathPrefix.
-  pathPrefix: "/paleolab", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteTitle: "Paleoekologie", // Site title.
+  siteUrl: "https://paleoekologie.cz", // Domain of your website without pathPrefix.
   siteDescription: `Oddělení paleoekologie (Botanický ústav AV ČR) se zabývá výzkumem dlouhodobé dynamiky vegetace a přírodního prostředí.`, // Website description used for RSS feeds/meta description tag.
 };
