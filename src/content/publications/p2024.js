536eea6d842958e39adaa63e5cad679ee4539130
Lenka <hubackova.lenka@gmail.com>
2024-08-09 22:23:38 +0200

    Update p2024.js

--- src/content/publications/p2024.js
+++ src/content/publications/p2024.js
@@ -2,5 +2,32 @@ import React from "react";
 import { ALink } from "../../components/atoms/StyledLink";
 import { Link } from "gatsby";
 
-export const impact = null;
+export const impact = (
+<>
+<p>
+  Szabó P., <Link to="/people/" state={{ person: "bobek" }}>Bobek P.</Link>, <Link to="/people/" state={{ person: "dudova" }}>Dudová L.</Link> &amp; Hédl R (2024): From oxen to tourists: The management history of subalpine grasslands in the Sudeten mountains and its significance for nature conservation. <em>The Anthropocene Review</em>: <a href="https://doi.org/10.1177/20530196241266227">doi:10.1177/20530196241266227</a>.
+</p>
+<p>
+  Klinkovská, K., &amp; <Link to="/people/" state={{ person: "rolecek" }}>Roleček, J.</Link> (2024): Floristic classification of Geranion sanguinei in South Moravia (Czech Republic). <em>Biologia</em>, 79(4), 1113-1127. <a href="https://doi.org/10.1007/s11756-023-01464-w">https://doi.org/10.1007/s11756-023-01464-w</a>.
+</p>
+<p>
+  Nezhad, M. T. K., Šamonil, P., Daněk, P., Jaroš, J., <Link to="/people/" state={{ person: "hajek" }}>Hájek, M.</Link>, <Link to="/people/" state={{ person: "hajkova" }}>Hájková, P.</Link>, ... &amp; <Link to="/people/" state={{ person: "rolecek" }}>Roleček, J.</Link> (2024). Lipid biomarkers and stable isotopes uncover paleovegetation changes in extremely species-rich forest-steppe ecosystems, Central Europe. <em>Environmental Research</em>, 119564. <a href="https://doi.org/10.1016/j.envres.2024.119564">https://doi.org/10.1016/j.envres.2024.119564</a>.
+</p>
+<p>
+  <Link to="/people/" state={{ person: "hajkova" }}>Hájková P.</Link>, Široká A., Petr L., <Link to="/people/" state={{ person: "jamrichova" }}>Jamrichová E.</Link> &amp; Peterka T. 2024: A complex insight into the Late Quaternary history of Bohemian Moravian Highlands summit. <em>Folia Geobotanica</em> 58, 127-150. <a href="https://doi.org/10.1007/s12224-023-09436-3">https://doi.org/10.1007/s12224-023-09436-3</a>.
+</p>
+<p>
+  Hošek J., Pokorný P., Storch D., Kvaček J., Havig J., Novák J., <Link to="/people/" state={{ person: "hajkova" }}>Hájková P.</Link>, <Link to="/people/" state={{ person: "jamrichova" }}>Jamrichová E.</Link>, Brengman L., Radoměřský T., Křížek M., Magna T., Rapprich V., Laufek F., Hamilton T., Pack A., Di Rocco T. &amp; Horáček I. 2024: Hot spring oases in the periglacial desert as the Last Glacial Maximum refugia for temperate trees in Central Europe. <em>Science Advances</em> 10, 1-14. <a href="https://www.science.org/doi/10.1126/sciadv.ado6611">https://www.science.org/doi/10.1126/sciadv.ado6611</a>.
+</p>
+<p>
+  Hajnalová M., Goláňová P., <Link to="/people/" state={{ person: "jamrichova" }}>Jamrichová E.</Link>, Petr L., Fránková M., Barta P., Kočárová R., Flammer P. &amp; Peto A. 2024: Land cover and use history of large empty spaces at fortified Iron Age hilltop sites, a case study from La Terrasse, Bibracte oppidum. <em>Vegetation History and Archaeobotany</em> 33, 269-288. <a href="https://doi.org/10.1007/s00334-023-00934-0">https://doi.org/10.1007/s00334-023-00934-0</a>.
+</p>
+<p>
+  Jiménez-Alfaro B., Auniņa L., Carbognani M., Dítě D., Fernández-Pascual E., Garbolino E., <Link to="/people/" state={{ person: "hajek" }}>Hájek O.</Link>, <Link to="/people/" state={{ person: "hajkova" }}>Hájková P.</Link>, Ivchenko T., Jandt U., Jansen F., Kolari T., Pawlikowski P., Pérez-Haase A., Peterka T., Petraglia A., Plesková Z., Tahvanainen T., Tomaselli M. &amp; <Link to="/people/" state={{ person: "hajek" }}>Hájek M.</Link> (2023). Habitat‐based biodiversity responses to macroclimate and edaphic factors in European fen ecosystems. <em>Global Change Biology</em>. <a href="https://doi.org/10.1111/gcb.16965">https://doi.org/10.1111/gcb.16965</a>.
+</p>
+<p>
+  Pokorná A., <Link to="/people/" state={{ person: "hajkova" }}>Hájková P.</Link>, Bernardová A., Jonášová B., Kučerová A., Jiroušková J., Šumberová K., Šolcová A., Starec P. &amp; Tichý L. (2024). A new tool for formalised vegetation reconstruction from (sub)fossil records – the FEVER Index. <em>Vegetation History and Archaeobotany</em>. 1-15. <a href="https://doi.org/10.1007/s00334-024-00996-8">https://doi.org/10.1007/s00334-024-00996-8</a>.
+</p>
+</>
+);
 export const others = null;
