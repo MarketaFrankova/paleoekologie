a5bd4c54bd8072e2cff51914397a7b7d8d9b3f49
Lenka <hubackova.lenka@gmail.com>
2024-08-09 22:17:55 +0200

    Update p2023.js

--- src/content/publications/p2023.js
+++ src/content/publications/p2023.js
@@ -2,5 +2,51 @@ import React from "react";
 import { ALink } from "../../components/atoms/StyledLink";
 import { Link } from "gatsby";
 
-export const impact = null;
+export const impact = (
+  <>
+<p>
+  Püntener D., Šamonil P., Tikhomirov D., Daněk P., Christl M., <Link to="/people/" state={{ person: "rolecek" }}>Roleček J.</Link> & Egli M. (2023). Soil erosion rates during the Holocene continuity in a forest-steppe landscape. <em>Earth Surface Processes and Landforms</em> 48: 504-524. <a href="https://doi.org/10.1002/esp.5499">https://doi.org/10.1002/esp.5499</a>
+</p>
+<p>
+  Jílek J., Rožnovský D. & <Link to="/people/" state={{ person: "jamrichova" }}>Jamrichová E.</Link> (2022). Roman bronze vessels from Jazovice in the district of Znojmo. A contribution to the interpretation of solitary finds of Roman bronze vessels. <em>Anthropologie</em> 60. <a href="https://doi.org/10.26720/anthro.22.05.17.1">https://doi.org/10.26720/anthro.22.05.17.1</a>
+</p>
+<p>
+  Lestienne M., <Link to="/people/" state={{ person: "jamrichova" }}>Jamrichová E.</Link>, Kuosmanen N., Diaconu A-C., Schafstall N., Goliáš V., Kletetschka G., Šulc V. & Kuneš P. (2023). Development of high diversity beech forest in the eastern Carpathians. <em>Journal of Biogeography</em> 50: 699-714. <a href="https://doi.org/10.1111/jbi.14562">https://doi.org/10.1111/jbi.14562</a>
+</p>
+<p>
+  Sim T., Swindles T. G., Morris P. J., & al. (incl. <Link to="/people/" state={{ person: "bobek" }}>Bobek P.</Link>) (2023). Regional variability in peatland burning at mid-to high-latitudes during the Holocene. <em>Quaternary Science Reviews</em> 305: 1-17. <a href="https://doi.org/10.1016/j.quascirev.2023.108020">https://doi.org/10.1016/j.quascirev.2023.108020</a>
+</p>
+<p>
+  Peterka T., <Link to="/people/" state={{ person: "hajkova" }}>Hájková P.</Link>, Jiroušek M., Hinterlang D., Chytrý M., Aunina L., Deme J., Lyons M., Seiler H., Zechmeister H., Apostolova I., Beierkuhnlein C., Bischof M., Biţă-Nicolae C., Brancaleoni L., Ćušterevska R., Dengler J., Didukh Y., Dítě D., Felbaba-Klushyna L., Garbolino E., Gerdol R., Iemelianova S., Jansen F., Juutinen R., Kamberović J., Kapfer J., Klímová B., Knollová I., Kolari T.H.M., Lazarević P., Luostarinen R., Mikulášková E., Milanović D., Miserere L., Moeslund J.E., Molina J.A., Pérez-Haase A., Petraglia A., Puglisi M., Ruprecht E., Šmerdová E., Spitale D., Tomaselli M., Vassilev K. & <Link to="/people/" state={{ person: "hajek" }}>Hájek M.</Link> (2023). Formalized classification of the class Montio-Cardaminetea in Europe: towards a consistent typology of spring vegetation. <em>Preslia</em> 95: 347-383. <a href="https://doi.org/10.23855/preslia.2023.347">https://doi.org/10.23855/preslia.2023.347</a>
+</p>
+<p>
+  Abraham V., Macek M., Tkáč P., Novák D., Pokorný P., Kozáková R., <Link to="/people/" state={{ person: "jamrichova" }}>Jamrichová E.</Link>, <Link to="/people/" state={{ person: "soukupova" }}>Soukupová M. G.</Link> & Kolář J. (2023). Pollen anthropogenic indicators revisited using large-scale pollen and archaeological datasets: 12,000 years of human-vegetation interactions in central Europe. <em>Preslia</em> 95: 385-411. <a href="https://doi.org/10.23855/preslia.2023.385">https://doi.org/10.23855/preslia.2023.385</a>
+</p>
+<p>
+  Badino F., Pini R., Ravazzi C., Chytrý M., Bertuletti P., Bortolini E., <Link to="/people/" state={{ person: "dudova" }}>Dudová L.</Link>, Peresani M., Romandini M. & Benazzi S. (2023). High-resolution ecosystem changes pacing the millennial climate variability at the Middle to Upper Palaeolithic transition in NE-Italy. <em>Scientific Reports</em> 13: 1-17. <a href="https://doi.org/10.1038/s41598-023-38081-1">https://doi.org/10.1038/s41598-023-38081-1</a>
+</p>
+<p>
+  Singh P., Jiroušek M., <Link to="/people/" state={{ person: "hajkova" }}>Hájková P.</Link>, Horsák M. & <Link to="/people/" state={{ person: "hajek" }}>Hájek M.</Link> (2023). The future of carbon storage in calcareous fens depends on the balance between groundwater discharge and air temperature. <em>Catena</em> 231: 1-10. <a href="https://doi.org/10.1016/j.catena.2023.107350">https://doi.org/10.1016/j.catena.2023.107350</a>
+</p>
+<p>
+  Radoměřský T., <Link to="/people/" state={{ person: "bobek" }}>Bobek P.</Link>, Man M., <Link to="/people/" state={{ person: "svitavska" }}>Svitavská Svobodová H.</Link> & Kuneš P. (2023). Modelling the location of interglacial microrefugia for cold-adapted species: insights from the terrain-mediated distribution of Rhododendron tomentosum in a temperate region in central Europe. <em>Preslia</em> 95: 267-296. <a href="https://doi.org/10.23855/preslia.2023.267">https://doi.org/10.23855/preslia.2023.267</a>
+</p>
+<p>
+  Abraham V., Man M., Theuerkauf M., Pokorný P., <Link to="/people/" state={{ person: "bobek" }}>Bobek P.</Link> & Novák J. (2023). Spatially explicit, quantitative reconstruction of past vegetation based on pollen or charcoal data as a tool for autecology of trees. <em>Landscape Ecology</em> 38: 1747-1763. <a href="https://doi.org/10.1007/s10980-023-01652-8">https://doi.org/10.1007/s10980-023-01652-8</a>
+</p>
+<p>
+  <Link to="/people/" state={{ person: "hoskova" }}>Hošková K.</Link>, Neustupa J., Pokorný P. & Pokorná A. (2022). Phylogenetic, ecological and intraindividual variability patterns in grass phytolith shape. <em>Annals of Botany</em> 129: 303-314. <a href="https://doi.org/10.1093/aob/mcab143">https://doi.org/10.1093/aob/mcab143</a>
+</p>
+<p>
+  Wiezik M., <Link to="/people/" state={{ person: "jamrichova" }}>Jamrichová E.</Link>, Máliš F., Beláňová E., Hrivnák R., <Link to="/people/" state={{ person: "hajek" }}>Hájek M.</Link> & <Link to="/people/" state={{ person: "hajkova" }}>Hájková P.</Link> (2023). Transformation of West-Carpathian primeval woodlands into high-altitude grasslands from as early as the Bronze Age. <em>Vegetation History and Archaeobotany</em> 32: 205-220. <a href="https://doi.org/10.1007/s00334-022-00896-9">https://doi.org/10.1007/s00334-022-00896-9</a>
+</p>
+<p>
+  Coufal R., <Link to="/people/" state={{ person: "hajkova" }}>Hájková P.</Link>, <Link to="/people/" state={{ person: "hajek" }}>Hájek M.</Link>, Jiroušek M., Polášek M., Horsáková V. & Horsák M. (2023). Compositional variation of endangered spring fen biota reflects within-site variation in soil temperature. <em>Plant and Soil</em> 485: 439-455. <a href="https://doi.org/10.1007/s11104-022-05841-3">https://doi.org/10.1007/s11104-022-05841-3</a>
+</p>
+<p>
+  Singh P., <Link to="/people/" state={{ person: "hajkova" }}>Hájková P.</Link>, Jiroušek M., Lizoňová Z., Peterka T., Plesková Z., Šímová A., <Link to="/people/" state={{ person: "smerdova" }}>Šmerdová E.</Link>, Štechová T. & <Link to="/people/" state={{ person: "hajek" }}>Hájek M.</Link> (2022). Can Sphagnum removal reverse the undesired succession of rich fens under different alkalinity and fertility levels? <em>Ecological Applications</em> 32: 1-15. <a href="https://doi.org/10.1002/eap.2691">https://doi.org/10.1002/eap.2691</a>
+</p>
+  </>
+);
+
 export const others = null;
